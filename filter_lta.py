f4419ae195d3b68dc613fb3c2003aa05107ef261
Shubhankar Deshpande <shubhankardeshpande@hotmail.com>
2017-01-15 07:33:49 +0530

    Modify filter parser to solve for edge cases

--- filter_lta.py
+++ filter_lta.py
@@ -14,6 +14,12 @@ for DIR_NAME in all_observations:
     """
     #Extract substring that contains obslog relative path
     relative_path = re.findall(r'[/][\d]+[.]obslog', current_obslog[0])[0][1:] 
+    #Invalid file (not fitting given constraints i.e. < 900 MHz and IF BW != 6,16,32)
     if relative_path not in valid_observations:
+        print data_dir+DIR_NAME
+    #Valid obslog file with no LTA file in the DIR
+    """
+    if relative_path in valid_observations:
         if glob.glob(data_dir+DIR_NAME+'/'+'*.lta') == []:
             print data_dir+DIR_NAME
+    """ 
